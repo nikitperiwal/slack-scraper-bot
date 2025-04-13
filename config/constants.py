f42460788ecda3fa3701a5454eddc5ccfade1653
Nikit.Periwal <nikit.periwal@razorpay.com>
2025-04-14 03:10:20 +0530

    reformatting

--- config/constants.py
+++ config/constants.py
@@ -1,6 +1,6 @@
 SLACK_BOT_TOKEN = "xoxb-your-slack-bot-token"
-SLACK_APP_TOKEN = "xapp-your-slack-app-token"
-SLACK_CHANNEL_NAME = "#your-channel-name"
+
+SLACK_CHANNEL_NAME = "#data_platform_support"
 DEFAULT_LOOKBACK_DAYS = 30
 SAVE_TO_DRIVE = False  # Set to True to upload to Google Drive
 
